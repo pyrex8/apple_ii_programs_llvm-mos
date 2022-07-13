d8b0e8b401ef0dfda9a1129362a14246abf2ba1d
pyrex8 <ev64bug@yahoo.com>
2022-07-12 21:20:10 -0700

    added clobber to inline assembly

--- hires_fill/hires_fill.c
+++ hires_fill/hires_fill.c
@@ -11,7 +11,7 @@
 
 #define CASSETTE_OUT        0xC020
 
-#define STROBE(x) asm("bit %0" :: "i"(x));
+#define STROBE(x) asm volatile ("bit %0" :: "i"(x));
 
 // Addresses
 enum Zero_page
@@ -29,7 +29,8 @@ enum Zero_page
 #define ADDR1H_P            *((uint8_t*)ADDR1H)
 
 
-static inline void pageset(uint8_t page, uint8_t value, uint8_t length)
+static inline void pageset(
+    uint8_t page, uint8_t value, uint8_t length)
 {
     // this method takes 114ms
     DATA1_P = value;
@@ -38,16 +39,16 @@ static inline void pageset(uint8_t page, uint8_t value, uint8_t length)
     ADDR1L_P = 0x00;
 
     // init registers with memory
-    asm volatile ("lda %0" :: "i"(DATA1));    // value to fill page(s) with
-    asm volatile ("ldx %0" :: "i"(DATA2));    // number of pages
-    asm volatile ("ldy %0" :: "i"(ADDR1L)); // address two bytes
+    asm volatile ("lda %0" :: "i"(DATA1) : "a");    // value to fill page(s) with
+    asm volatile ("ldx %0" :: "i"(DATA2) : "x");    // number of pages
+    asm volatile ("ldy %0" :: "i"(ADDR1L) : "y"); // address two bytes
 
     // nested loops
-    asm volatile ("1: sta (%0),y" :: "i"(ADDR1L));
-    asm volatile ("iny");
+    asm volatile ("1: sta (%0),y" :: "i"(ADDR1L) : "a", "memory");
+    asm volatile ("iny" ::: "y");
     asm volatile ("bne 1b");
-    asm volatile ("inc %0" :: "i"(ADDR1H));
-    asm volatile ("dex");
+    asm volatile ("inc %0" :: "i"(ADDR1H) : "memory");
+    asm volatile ("dex"::: "x");
     asm volatile ("bne 1b");
 }
 
@@ -55,12 +56,12 @@ int main(void)
 {
     STROBE(TXTCLR);
     STROBE(HIRES);
-    STROBE(CASSETTE_OUT);
-    pageset(HGR1SCRN_PAGE, WHITE, HGRSCRN_LENGTH);
-    STROBE(CASSETTE_OUT);
     while(1)
     {
-
+        STROBE(CASSETTE_OUT);
+        pageset(HGR1SCRN_PAGE, WHITE, HGRSCRN_LENGTH);
+        STROBE(CASSETTE_OUT);
+        pageset(HGR1SCRN_PAGE, BLACK, HGRSCRN_LENGTH);
     }
     return 0;
 }
